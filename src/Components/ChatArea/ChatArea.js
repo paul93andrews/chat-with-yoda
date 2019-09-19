ba87077289b2f2ddd7b4bdd461fb0e70349887b0
Paul <paulandrews810@gmail.com>
2019-09-19 18:35:25 -0400

    styling completed

--- src/Components/ChatArea/ChatArea.js
+++ src/Components/ChatArea/ChatArea.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SendMessage from '../SendMessage/SendMessage';
+import './_chatArea.scss';
 
 class ChatArea extends Component {
     constructor(){
@@ -29,37 +30,41 @@ class ChatArea extends Component {
             <section className="chatBox">
                 {/* ternary operator below checks if there is a user signed in and will display different chatBox designs depending on which is true */}
                 {this.props.userID == ''
-                ? true
-                : <section className="chatArea">
-                    <div className="messageArea" id="chat">
-                        {this.props.userMessages.map((convo, index) => {
-                            return (
-                                <ul id="chatHistory" key={index}>
-                                    <li>
-                                        <p>{
-                                            this.props.userName == null
-                                                ? 'Guest: '
-                                                : convo.pastMessages.name + ': '}
-                                            {convo.pastMessages.userMessage}
-                                        </p>
-                                        <p>
-                                            {index !== this.props.userMessages.length - 1
-                                                ? 'Yoda: ' + convo.pastMessages.yodaQuote
-                                                : this.state.yodaDelayFinished ? 'Yoda: ' + convo.pastMessages.yodaQuote : "..."
-                                            }
-                                        </p>
-                                    </li>
-                                </ul>
-                            );
-                        })}
-                    </div>
+                ? <section className="chatArea">
+                        <img src={require("../images/lightsaber.png")} alt="Floating lightsaber" className="vibrate-2 lightsaber"/> 
+                    </section>
+                : <div>
+                    <section className="chatArea">
+                        <div className="messageArea" id="chat">
+                            {this.props.userMessages.map((convo, index) => {
+                                return (
+                                    <ul id="chatHistory" key={index}>
+                                        <li>
+                                            <p>{
+                                                this.props.userName == null
+                                                    ? 'Guest: '
+                                                    : convo.pastMessages.name + ': '}
+                                                {convo.pastMessages.userMessage}
+                                            </p>
+                                            <p>
+                                                {index !== this.props.userMessages.length - 1
+                                                    ? 'Yoda: ' + convo.pastMessages.yodaQuote
+                                                    : this.state.yodaDelayFinished ? 'Yoda: ' + convo.pastMessages.yodaQuote : "..."
+                                                }
+                                            </p>
+                                        </li>
+                                    </ul>
+                                );
+                            })}
+                        </div>
+                    </section>
                     <SendMessage
                         userID={this.props.userID}
                         delayYoda={this.delayYodaMessage}
                         showYoda={this.showYodaMessage}
                         userName={this.props.userName}
                     />
-                </section>
+                </div>
                 }
             </section>
         )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import './starwars-glyphicons/fonts/starwars-glyphicons-webfont.ttf'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
